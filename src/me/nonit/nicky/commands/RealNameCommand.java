097b6ee301abc66f87e9c973e3bf616b78f79d53
Daniel Murray <daniel@nonit.me>
2015-04-11 23:25:18 +0100

    Fixes and updates

--- src/me/nonit/nicky/commands/RealNameCommand.java
+++ src/me/nonit/nicky/commands/RealNameCommand.java
@@ -9,7 +9,6 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.*;
 
